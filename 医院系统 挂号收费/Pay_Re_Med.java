a492caec748350464b2cbd2be59e24864b746013
gelatoXU <1511477553@qq.com>
2016-12-24 14:20:12 +0800

    医院系统 挂号收费

---
+++ 医院系统 挂号收费/Pay_Re_Med.java
@@ -0,0 +1,136 @@
+package Register_Charge;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.net.*;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import all_class.*;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class Pay_Re_Med extends JFrame {
+	private static Case case2;
+	private static ArrayList<Case> Register_Charged;//用于挂号端的收费
+	private JPanel contentPane;
+	static int j=0;
+	private static JLabel lblPay;
+	private static JLabel lblName;
+	static int i=0;
+	private static Socket socket;
+	private static Account account;
+	 private  static Global_info global_info;
+	
+	 static int page=0;
+	 
+	 public static void up_load()
+	 {
+		 TCP tcp=new TCP();
+		 tcp.setCase_toStore(Register_Charged);
+	 }
+	 public static void GetData()
+		{
+			TCP tcp =new TCP();
+			global_info=tcp.get_global_info();
+			Register_Charged=tcp.GetCase_toCharge();	
+		}
+	public Pay_Re_Med() {
+		if(page==0)
+		{
+			GetData();
+		}
+		case2=Register_Charged.get(page);
+		case2.getPi().getName();
+		case2.getCharge().getSum_fee();
+		lblName.setText("病人需要缴费 " +case2.getPi().getName());
+		lblPay.setText("病人需要缴费 " +case2.getCharge().getSum_fee());
+		case2.getCharge().setPay(true);
+		//处理完成的病人都将修改写回服务器
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(0, 0, 424, 35);
+		contentPane.add(panel);
+		
+		JLabel label = new JLabel("--------\u6536\u8D39-------");
+		panel.add(label);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBounds(0, 34, 424, 25);
+		contentPane.add(panel_1);
+		
+		JLabel lblName = new JLabel("New label");
+		panel_1.add(lblName);
+		
+		JPanel panel_2 = new JPanel();
+		panel_2.setBounds(0, 69, 424, 25);
+		contentPane.add(panel_2);
+		
+		JLabel lblPay = new JLabel("New label");
+		panel_2.add(lblPay);
+		
+		JPanel panel_3 = new JPanel();
+		panel_3.setBounds(0, 104, 424, 35);
+		contentPane.add(panel_3);
+		
+		JPanel panel_4 = new JPanel();
+		panel_4.setBounds(0, 139, 424, 35);
+		contentPane.add(panel_4);
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		lblNewLabel.setVisible(false);
+		panel_4.add(lblNewLabel);
+		
+		JPanel panel_5 = new JPanel();
+		panel_5.setBounds(0, 216, 424, 35);
+		contentPane.add(panel_5);
+		
+		JButton button_1 = new JButton("\u9000\u51FA");
+		button_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				dispose();
+			}
+		});
+		panel_5.add(button_1);
+		
+		JPanel panel_6 = new JPanel();
+		panel_6.setBounds(0, 184, 424, 35);
+		contentPane.add(panel_6);
+		
+		JButton button = new JButton("\u786E\u8BA4\u7F34\u8D39");
+		button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(i<Register_Charged.size())
+				{
+					dispose();
+					Pay_Re_Med frame = new Pay_Re_Med();
+					frame.setVisible(true);
+					i++;
+				}
+				else 
+				{
+					lblNewLabel.setVisible(true);
+					lblNewLabel.setText("所有人员收费完成");
+					up_load();
+				}
+			
+				
+			}
+		});
+		panel_6.add(button);
+	}
+}
---
+++ 医院系统 挂号收费/Register.java
@@ -0,0 +1,113 @@
+package Register_Charge;
+import java.util.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.*;
+import java.net.*;
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import all_class.*;
+
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.JSplitPane;
+import javax.swing.JScrollPane;
+
+public class Register extends JFrame {
+	static int i=0;
+	private static  Account account;
+	private static Socket socket;
+	private static Global_info global_info;
+    private static ObjectInputStream in;
+    private static ObjectOutputStream out;
+    private static JFrame frame;
+	private JPanel contentPane;
+	/**
+	 * Launch the application.
+	 */
+	
+	public static void frist_set(){
+		try {
+			ObjectOutputStream os=new ObjectOutputStream(socket.getOutputStream());
+			os.writeObject(account);
+			os.flush();
+			os.close();
+			socket.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			System.out.println("无法连接服务器");
+		}
+	}
+	
+	public Register() {
+		
+	    frist_set();
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(0, 47, 434, 34);
+		contentPane.add(panel);
+		
+		JLabel lblNewLabel_1 = new JLabel("");
+		panel.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("\u6536\u8D39\u4EBA\u5458");
+		panel.add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_3 = new JLabel("MissLi");
+		panel.add(lblNewLabel_3);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBounds(0, 91, 434, 50);
+		contentPane.add(panel_1);
+		
+		JButton btnReg = new JButton("\u6302\u53F7");
+		btnReg.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Registration registration=new Registration();
+				registration.setVisible(true);
+			}
+		});
+		panel_1.add(btnReg);
+		
+		JLabel label = new JLabel("");
+		label.setBounds(174, 22, 54, 15);
+		contentPane.add(label);
+		
+		JPanel panel_2 = new JPanel();
+		panel_2.setBounds(0, 0, 434, 44);
+		contentPane.add(panel_2);
+		
+		JLabel lblNewLabel = new JLabel("\u6B22\u8FCE\u8FDB\u5165\u6302\u53F7\u6536\u8D39\u7AEF");
+		panel_2.add(lblNewLabel);
+		
+		JPanel panel_3 = new JPanel();
+		panel_3.setBounds(0, 150, 434, 34);
+		contentPane.add(panel_3);
+		
+		JButton btnReg_Pay = new JButton("\u6302\u53F7\u6536\u8D39");
+		btnReg_Pay.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Pay_Re_Med frame = new Pay_Re_Med();
+				frame.setVisible(true);
+			}
+		});
+		panel_3.add(btnReg_Pay);
+		
+		JPanel panel_4 = new JPanel();
+		panel_4.setBounds(0, 203, 434, 58);
+		contentPane.add(panel_4);
+			
+	}
+}
